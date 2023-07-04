6fb0eec578598334bb44658bcfbd8e9b0895aa7a
Neha <nehatualea@gmail.com>
2023-07-04 14:05:39 +0530

    added:  validation schema

---
+++ src/utils/mock.js
@@ -0,0 +1,142 @@
+import { v4 as uuidv4 } from 'uuid';
+export const PROGRAMS=[
+    {   
+
+        id:uuidv4(),
+        name:"Body Building",
+       url:"https://images.pexels.com/photos/3837743/pexels-photo-3837743.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+    },
+    {
+        id:uuidv4(),
+        name:"Yoga",
+        url:"https://images.pexels.com/photos/6111582/pexels-photo-6111582.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    },
+
+    {
+        id:uuidv4(),
+        name:"flexibility",
+        url:"https://images.pexels.com/photos/4587402/pexels-photo-4587402.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    },
+    {
+        id:uuidv4(),
+        name:"Push-Ups",
+        url:"https://images.pexels.com/photos/176782/pexels-photo-176782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+
+    }
+    ,
+    {
+        id:uuidv4(),
+        name:"Boxing",
+        url:"https://images.pexels.com/photos/6740347/pexels-photo-6740347.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    }
+    ,
+    {
+        id:uuidv4(),
+        name:"Zumba",
+        url:"https://images.pexels.com/photos/4090009/pexels-photo-4090009.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    }
+    ,
+
+    {
+        id:uuidv4(),
+        name:"Rehabilitations",
+        url:"https://images.pexels.com/photos/6111581/pexels-photo-6111581.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:uuidv4(),
+        name:"Weight-loss",
+        url:"https://images.pexels.com/photos/6551098/pexels-photo-6551098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    }
+    ,
+
+    {
+        id:uuidv4(),
+        name:"Pull-up",
+        url:"https://images.pexels.com/photos/7697768/pexels-photo-7697768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    }
+    ,
+
+    {
+        id:uuidv4(),
+      name:"Aerobic",
+        url:"https://images.pexels.com/photos/866021/pexels-photo-866021.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    }
+
+]
+
+
+export const TRAINERS = [
+    {
+      id: uuidv4(),
+      name: "John Doe",
+      about: "Certified personal trainer specializing in bodybuilding and strength training.",
+      image_url: "https://images.pexels.com/photos/4854282/pexels-photo-4854282.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Jane Smith",
+      about: "Experienced yoga instructor skilled in various yoga styles and meditation techniques.",
+      image_url: "https://images.pexels.com/photos/6740058/pexels-photo-6740058.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Michael Johnson",
+      about: "Certified fitness coach specializing in flexibility training and injury prevention.",
+      image_url: "https://images.pexels.com/photos/6111621/pexels-photo-6111621.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Emily Davis",
+      about: "Expert trainer focusing on functional training and high-intensity workouts.",
+      image_url: "https://images.pexels.com/photos/3912516/pexels-photo-3912516.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Mark Thompson",
+      about: "Specialized in cardiovascular exercises and endurance training.",
+      image_url: "https://images.pexels.com/photos/6456298/pexels-photo-6456298.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Sarah Wilson",
+      about: "Experienced trainer with expertise in postnatal fitness and women's health.",
+      image_url: "https://images.pexels.com/photos/6550826/pexels-photo-6550826.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Ryan Thompson",
+      about: "Certified strength and conditioning specialist with a focus on sports performance.",
+      image_url: "https://images.pexels.com/photos/3912953/pexels-photo-3912953.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Jessica Adams",
+      about: "Experienced trainer skilled in weight loss programs and nutrition planning.",
+      image_url: "https://images.pexels.com/photos/4058325/pexels-photo-4058325.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "David Roberts",
+      about: "Specialized in functional movement training and corrective exercises.",
+      image_url: "https://images.pexels.com/photos/6456019/pexels-photo-6456019.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Olivia Brown",
+      about: "Experienced trainer focusing on Pilates and core strength training.",
+      image_url: "https://images.pexels.com/photos/6455819/pexels-photo-6455819.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Andrew Wilson",
+      about: "Certified boxing instructor with expertise in high-intensity boxing workouts.",
+      image_url: "https://images.pexels.com/photos/4587408/pexels-photo-4587408.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: uuidv4(),
+      name: "Laura Davis",
+      about: "Expert in Zumba and dance fitness, providing fun and energetic workout sessions.",
+      image_url: "https://images.pexels.com/photos/7207556/pexels-photo-7207556.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  ];
+  
