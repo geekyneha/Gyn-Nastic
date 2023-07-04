8c0053ad237e6c29a66fcabe71cb24bf1b0c41c9
Neha <nehatualea@gmail.com>
2023-07-05 04:49:59 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -19,13 +19,18 @@ import SignUp from "./components/pages/Signup/SignUp";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Header />}>
+      <>
+        <Route path="/" element={<Header />}>
         <Route index element={<AppLayout />}></Route>
-        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signup" element={<SignUp />} />
+        
         <Route path="/view-team" element={<Trainers />} />
       </Route>
+       <Route path="/login" element={<Login />} />
+       <Route path="/signup" element={<SignUp />} />
+       
+        </>
+    
     )
   );
 
