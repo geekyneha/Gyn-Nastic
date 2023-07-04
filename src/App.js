46138ab09fddf562efa34be4800c15716b05038e
Neha <nehatualea@gmail.com>
2023-07-04 13:58:34 +0530

    Build: App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+import Header from './components/sections/header/Header'
+import AppLayout from "./components/pages/AppLayout";
+import {Outlet} from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom";
+
+import About from "./components/pages/About/About"
+import Hero from './components/sections/Hero/hero'
+import Pricing from './components/sections/pricing/PricingSec';
+
+import Login from './components/pages/Login/Login';
+import Trainers from './components/pages/Trainers/Trainers';
+import SignUp from './components/pages/Signup/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Header />}>
+       <Route index element={<AppLayout />}></Route>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/signup" element={<SignUp />}/>
+      <Route path="/view-team" element={<Trainers />}/>
+    </Route>
+  ))
+
+return (
+      <div className="App">
+       
+   
+      <RouterProvider router={router} />
+       
+    
+
     </div>
   );
 }
 
+
 export default App;
+
